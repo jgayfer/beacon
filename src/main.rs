965a3b91252a6f0b94563889ee731aac5668aa5f
James Gayfer <10660608+jgayfer@users.noreply.github.com>
2026-09-22 11:07:32 -0700

    Setup basic 2d Bevy app

--- src/main.rs
+++ src/main.rs
@@ -1,1 +1,12 @@
-fn main() {}
+use bevy::prelude::*;
+
+fn main() {
+    App::new()
+        .add_plugins(DefaultPlugins)
+        .add_systems(Startup, spawn_camera)
+        .run();
+}
+
+fn spawn_camera(mut commands: Commands) {
+    commands.spawn(Camera2d);
+}
